ae03142023a46e8be509cd7a98b2e84d193b34b2
Alex Fang <deeno@qq.com>
2023-04-04 17:01:24 +0800

    fix: fix release full paths resolver

---
+++ src/helpers/config.ts
@@ -0,0 +1,65 @@
+import { isNil, castArray } from "lodash";
+import * as fs from "./fs.js";
+
+export enum GitHubPagesPathEnum {
+  ROOT = "/",
+  DOCS = "/docs",
+}
+
+export type GitHubPagesPathValue =
+  | GitHubPagesPathEnum.ROOT
+  | GitHubPagesPathEnum.DOCS;
+
+export interface GitHubPagesOptions {
+  src: string;
+  ghpBranch: string;
+  ghpPath: GitHubPagesPathValue;
+  message: string;
+  cleanupGlob: string | string[];
+}
+
+function getBase(cwd?: string): string {
+  return cwd || process.cwd();
+}
+
+export async function getSrc(
+  opts: GitHubPagesOptions,
+  cwd?: string
+): Promise<string> {
+  const src = opts.src;
+  const base = getBase(cwd);
+  return await fs.resolve(src, base);
+}
+
+export function getGhpBranch(opts: GitHubPagesOptions): string {
+  return opts.ghpBranch || "gh-pages";
+}
+
+export function validateGhpPath(opts: GitHubPagesOptions): boolean {
+  const ghpPath = opts.ghpPath;
+  return (
+    ghpPath === GitHubPagesPathEnum.ROOT || ghpPath === GitHubPagesPathEnum.DOCS
+  );
+}
+
+export async function getGhpPath(
+  opts: GitHubPagesOptions,
+  cwd?: string
+): Promise<string> {
+  const ghpPath = opts.ghpPath;
+  const base = getBase(cwd);
+  return await fs.resolve(base, `.${ghpPath}`);
+}
+
+export function getMessage(opts: GitHubPagesOptions): string {
+  return opts.message || "chore: GitHub Pages release";
+}
+
+export function getCleanupGlob(opts: GitHubPagesOptions): string[] {
+  const cleanupGlob = opts.cleanupGlob;
+  if (isNil(cleanupGlob)) {
+    return ["./**/*", "!.github", "!.git*", "!./node_modules"];
+  }
+
+  return castArray(cleanupGlob);
+}
